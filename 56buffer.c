f228c0e80c19b5ee1fd4d226027205a6ca0fba4e
BQzhang-hub <BQ_zhang@163.com>
2020-07-05 19:15:57 +0800

    code_2

--- 56buffer.c
+++ 56buffer.c
@@ -27,25 +27,3 @@ void UC(){
     close(fd);
 
 }
-//程序员可以自己加buffer
-void UC_buffer(){
-    int fd = open("uc_buffer.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
-    if(fd == -1){
-        perror("open");
-        exit(-1);
-    }
-    int arr[10000]={};
-    for(i=0;i<1000000;++i){
-        arr[i%10000]=i;    //i%10000:0-9999
-        if(i%10000==9999)  //满缓冲区才写
-        write(fd,arr,sizeof(arr));
-    }
-    close(fd);
-
-}
-int main(){
-    //Std_C();
-    UC();
-    //UC_buffer();
-    return 0;
-}
